5779277d2c15772c33e163d5be1509be12c55661
Samarth-Manch <samarth.nimbargi@manchtech.com>
2025-04-16 20:54:59 +0530

    initialized project

---
+++ src/test/java/com/manch/launchpad/LaunchpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manch.launchpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
