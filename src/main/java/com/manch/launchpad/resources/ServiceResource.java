de19fdaca0d4e18ec56ad69bf8f64da9a32a2130
Samarth-Manch <samarth.nimbargi@manchtech.com>
2025-04-17 02:28:29 +0530

    updated port service resources

--- src/main/java/com/manch/launchpad/resources/ServiceResource.java
+++ src/main/java/com/manch/launchpad/resources/ServiceResource.java
@@ -4,20 +4,25 @@ import com.manch.launchpad.commons.responses.LaunchpadResponse;
 import com.manch.launchpad.models.request.PortModel;
 import com.manch.launchpad.models.request.ServiceModel;
 import com.manch.launchpad.models.request.VolumeModel;
+import com.manch.launchpad.services.PortService;
 import com.manch.launchpad.services.ServicesService;
+import com.manch.launchpad.services.VolumeService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/service")
 @AllArgsConstructor
 public class ServiceResource {
     ServicesService servicesService;
+    VolumeService volumeService;
+    PortService portService;
 
     @RequestMapping(method = RequestMethod.POST)
     public LaunchpadResponse<ServiceModel> createService(@RequestBody ServiceModel serviceModel) {
-        ServiceModel service = servicesService.createService(serviceModel);
-        return LaunchpadResponse.ok(service);
+        return LaunchpadResponse.ok(servicesService.createService(serviceModel));
     }
 
     @RequestMapping(path = "/{serviceId}", method = RequestMethod.GET)
@@ -27,31 +32,31 @@ public class ServiceResource {
 
     @RequestMapping(path = "/volumes", method = RequestMethod.POST)
     public LaunchpadResponse<VolumeModel> createVolume(@RequestBody VolumeModel volumeModel) {
-        return null;
+        return LaunchpadResponse.ok(volumeService.createVolume(volumeModel));
     }
 
     @RequestMapping(path = "/volumes/{serviceId}", method = RequestMethod.GET)
-    public LaunchpadResponse<VolumeModel> getVolumes(@PathVariable String serviceId) {
-        return null;
+    public LaunchpadResponse<List<VolumeModel>> getVolumes(@PathVariable String serviceId) {
+        return LaunchpadResponse.ok(volumeService.getAllVolumesByServiceId(serviceId));
     }
 
     @RequestMapping(path = "/volumes/{volumeId}", method = RequestMethod.GET)
     public LaunchpadResponse<VolumeModel> getVolume(@PathVariable String volumeId) {
-        return null;
+        return LaunchpadResponse.ok(volumeService.getVolume(volumeId));
     }
 
     @RequestMapping(path = "/ports", method = RequestMethod.POST)
     public LaunchpadResponse<PortModel> createPort(@RequestBody PortModel portModel) {
-        return null;
+        return LaunchpadResponse.ok(portService.createPort(portModel));
     }
 
     @RequestMapping(path = "/ports/{portId}}", method = RequestMethod.GET)
     public LaunchpadResponse<PortModel> getPort(@PathVariable String portId) {
-        return null;
+        return LaunchpadResponse.ok(portService.getPort(portId));
     }
 
     @RequestMapping(path = "/volumes/{serviceId}", method = RequestMethod.GET)
-    public LaunchpadResponse<VolumeModel> getPorts(@PathVariable String serviceId) {
-        return null;
+    public LaunchpadResponse<List<PortModel>> getPorts(@PathVariable String serviceId) {
+        return LaunchpadResponse.ok(portService.getPortsByServiceId(serviceId));
     }
 }
